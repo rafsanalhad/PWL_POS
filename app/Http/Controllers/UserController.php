882c96b3a23c3e50f564fd7fdb480b4e8b12c50b
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-03-14 10:49:41 +0700

    Hasil Praktikum-2.5 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,15 +12,37 @@ class UserController extends Controller
     {
         // $user = UserModel::where('level_id', 2)->count();
         // return view('user', ['data' => $user]);
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2,
             ],
         );
+
+        $user->username = 'manager12';
         $user->save();
-        return view('user', ['data' => $user]);
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
+        // $user->username = 'manager56';
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama', 'username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama', 'username']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+        // dd($user->isDirty());
     }
 }
