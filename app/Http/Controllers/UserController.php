e9419fb5fd66ba776351c278d6959a48953b1c71
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-03-14 10:07:33 +0700

    Hasil Praktikum- 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,16 +8,23 @@ use App\Models\UserModel;
 
 class UserController extends Controller
 {
-    public function index() {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        // UserModel::insert($data);
-        UserModel::create($data);
-        $user = UserModel::all();
-        return view ('user', ['data' => $user]);
+    public function index()
+    {
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
     }
 }
