e7e9abeb5326f2e2dcc287c2a6927c355a367f1a
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-03-14 11:33:03 +0700

    Hasil Praktikum-2.7 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
-    public function tambah(){
+    public function tambah()
+    {
         return view('user_tambah');
     }
-    public function tambah_simpan(Request $request){
+    public function tambah_simpan(Request $request)
+    {
         UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
@@ -26,11 +28,13 @@ class UserController extends Controller
 
         return redirect('/user');
     }
-    public function ubah($id){
+    public function ubah($id)
+    {
         $user = UserModel::find($id);
         return view('user_ubah', ['data' => $user]);
     }
-    public function ubah_simpan($id, Request $request){
+    public function ubah_simpan($id, Request $request)
+    {
         $user = UserModel::find($id);
         $user->username = $request->username;
         $user->nama = $request->nama;
@@ -40,7 +44,8 @@ class UserController extends Controller
 
         return redirect('/user');
     }
-    public function hapus($id){
+    public function hapus($id)
+    {
         $user = UserModel::find($id);
         $user->delete();
 
